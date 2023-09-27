228b36bbc80d4df2cae0c322dc33274fc4df3f3b
laxleary <133721313+laxleary@users.noreply.github.com>
2023-09-26 21:09:08 -0400

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,36 @@
+
+CREATE DATABASE "Zillow-Data"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
+DROP TABLE IF EXISTS Regions;
+DROP TABLE IF EXISTS Indicators;
+DROP TABLE IF EXISTS Data;
+
+
+CREATE TABLE Regions (
+    region_id varchar PRIMARY KEY,
+    region_type varchar NOT NULL,
+    region varchar NOT NULL
+);
+
+CREATE TABLE Indicators (
+    indicator_id varchar PRIMARY KEY,
+    indicator varchar NOT NULL,
+    category varchar NOT NULL
+);
+
+CREATE TABLE Data (
+    indicator_id varchar NOT NULL, 
+    FOREIGN KEY (indicator_id)
+    REFERENCES Indicators(indicator_id),
+    region_id varchar NOT NULL,
+    FOREIGN KEY (region_id)
+    REFERENCES Regions(region_id),
+    "date" date NOT NULL,
+    "value" float NOT NULL,
+    CONSTRAINT data_id PRIMARY KEY (indicator_id, region_id, "date")
+);
