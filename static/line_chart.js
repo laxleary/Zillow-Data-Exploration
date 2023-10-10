69630874ef334b1e62c56316a421e550e89990f1
laxleary <133721313+laxleary@users.noreply.github.com>
2023-10-10 17:40:53 -0400

    line_chart error fix

--- static/line_chart.js
+++ static/line_chart.js
@@ -854,5 +854,5 @@ const myChart = new Chart(
 
 //labels: ['1996', '1997', '1998', '1999', '2000', '2001', '2002','2003', '2004', '2005', '2006', '2007', '2008', '2009','2010','2011','2012','2013','2014','2015','2016','2017','2018','2019','2020','2021','2022','2023'],
 // Instantly assign Chart.js version
-const chartVersion = document.getElementById('chartVersion');
-chartVersion.innerText = Chart.version;
+// const chartVersion = document.getElementById('chartVersion');
+// chartVersion.innerText = Chart.version;
